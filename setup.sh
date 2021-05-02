86cf4211b74f4a1ee3f6f81f7b3003d2280fcb42
mosemister <nekronmoseley@gmail.com>
2021-05-02 18:30:03 +0100

    Added setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Minecraft-Ships/ShipsToSponge.git
+git clone https://github.com/Minecraft-Ships/ShipsToBukkit.git
+git clone https://github.com/Minecraft-Ships/ShipsForCore.git
+git clone https://github.com/Minecraft-Ships/ShipsCore.git
+git clone https://github.com/Minecraft-Ships/CoreToSponge.git
+git clone https://github.com/Minecraft-Ships/CoreToPaper.git
+git clone https://github.com/Minecraft-Ships/CoreToBukkit.git
